356524c75131a6b57a707688cf9dc8b50dc9043f
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-04-08 14:33:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import pyautogui, time
+import pyautogui, time, sys
+from python_imagesearch.imagesearch import imagesearch
 
-def goruntu_tanilama(file, loop):
-    start = pyautogui.locateOnScreen(file) # İmajı işleyip ekranımızda seçiyoruz
-    pyautogui.moveTo(start) # Ekranımızda bulunan konuma mausumuzu otomatik götürüyoruz
-    pyautogui.click(button='left') # Sol tık yaptırıyoruz
-    time.sleep(loop) # x saniye kadar mola
+def goruntu_isleme_opencv(file, cooldown):
+    pos = imagesearch(file)
+    if pos[0] != -1:
+        pyautogui.moveTo(pos[0], pos[1]) # move mouse to image
+        pyautogui.click(button='left') # Left click
+        print(pos) # print position
+        time.sleep(cooldown) # cooldown
+    else:
+        print("image not found") # image not found
 
-while(True):
-    goruntu_tanilama('./num_9.png', 4) # Sürekli dönen tanıma işlemi 
+try:
+    while(True):
+        goruntu_isleme_opencv('./num_9.png', 2)
+except goruntu_isleme_opencv:
+    sys.exit(1)
