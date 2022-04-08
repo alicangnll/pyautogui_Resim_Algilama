cbb8c5e4e8cda98e0cf80e9e5c34eb57b9d4b93e
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-04-08 03:36:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pyautogui, time
 
 def goruntu_tanilama(file, loop):
-    start = pyautogui.locateOnScreen(file)
-    pyautogui.moveTo(start)
-    pyautogui.click(button='left')
-    time.sleep(loop)
+    start = pyautogui.locateOnScreen(file) # İmajı işleyip ekranımızda seçiyoruz
+    pyautogui.moveTo(start) # Ekranımızda bulunan konuma mausumuzu otomatik götürüyoruz
+    pyautogui.click(button='left') # Sol tık yaptırıyoruz
+    time.sleep(loop) # x saniye kadar mola
 
 while(True):
-    goruntu_tanilama('./num_9.png', 4)
+    goruntu_tanilama('./num_9.png', 4) # Sürekli dönen tanıma işlemi 
